32d3785f13fe03a6d7ae6fa82451cad3a16ea4ea
emmanyouwell <emingala02@gmail.com>
2025-07-11 01:59:36 +0800

    Updated favicon.ico and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "TCHMB Portal Mobile Demo",
+  title: "TCHMB Live Demo",
   description: "Live screen sharing and livestreaming demo",
 };
 
